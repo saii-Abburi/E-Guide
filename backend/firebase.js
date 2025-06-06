19ac9c0cee1f7b9ace47cf174dbf0dfa3b71745d
SaiPraveenAbburi <147138953+saii-Abburi@users.noreply.github.com>
2025-06-06 12:52:49 +0530

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse('./serviceAccountKey.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
