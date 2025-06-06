fcda5488c1b7eac28fe866086207e8a3d467829e
SaiPraveenAbburi <147138953+saii-Abburi@users.noreply.github.com>
2025-06-06 12:14:37 +0530

    Update firebase.js

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || ""
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAETHlcsTPIy1ciOWOUrB12s2aQqYNPb-w",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "e-guide-4e170.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "e-guide-4e170",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "e-guide-4e170.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "688892710458",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:688892710458:web:e035ddb6067f3c7bce2f03",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ||"G-0WHKKZF078",
 };
 
 // Initialize Firebase
